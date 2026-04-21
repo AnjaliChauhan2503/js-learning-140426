e9a1325b3f353206fbf8bc352a42470a8473f501
AnjaliChauhan2503 <161436848+AnjaliChauhan2503@users.noreply.github.com>
2026-04-21 17:45:28 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,27 @@
+ "use strict";   // treat all JS code as newer version
+
+ // alert(3+3);  // we are using nodejs, not browser
+
+
+ let name ="Anjali"
+ let age=26
+ let isLoggedIn =false
+ let state;
+
+
+ // number => 2 to power 53
+ // bigint
+ // string => ""
+ // boolean => true/false
+ // null => standalone value
+ // undefined =>
+ // symbol => unique
+
+ // object 
+
+ //console.log(typeof null)   null is a object
+ //console.log(typeof undefined)   undefined is undefined type
+
+ //console.log(typeof age);
+ 
+ 
